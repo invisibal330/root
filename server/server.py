0ba4f2079c91278ea2ccc26efaf0a0fede912fcf
Bilal Haider ID <114842288+BilalHaiderID@users.noreply.github.com>
2023-01-05 01:55:13 +0500

    Create server.py

---
+++ server/server.py
@@ -0,0 +1,9 @@
+from requests import get
+from os import system as cmd
+from sys import exit as exit
+from os import aegv as arg
+from os import remove as rm
+
+def securitychack():pass
+def updatechack():pass
+def mainchack():pass
