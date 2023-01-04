0795fae33395fa929d65fedc611631a56d44f99a
Bilal Haider ID <114842288+BilalHaiderID@users.noreply.github.com>
2023-01-05 01:56:30 +0500

    Add files via upload

---
+++ data/modules.py
@@ -0,0 +1,7 @@
+from os import system as cmd
+from os import listdir as lst
+from os import mkdir as mkdr
+from os import remove as rmov
+from time import sleep as slp
+from bs4 import BeautifulSoup as BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
