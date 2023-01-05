75f87a2de68387744d1baf2b2d97024600f59ea0
Bilal Haider ID <114842288+BilalHaiderID@users.noreply.github.com>
2023-01-05 14:27:53 +0500

    Update modules.py

--- data/modules.py
+++ data/modules.py
@@ -1,6 +1,8 @@
 from os import system as cmd
 from os import listdir as lst
 from os import mkdir as mkdr
+from requests import post as post
+from requests import get as get
 from os import remove as rmov
 from time import sleep as slp
 from bs4 import BeautifulSoup as BeautifulSoup
